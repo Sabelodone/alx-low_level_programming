e1a636b14b8724f0a4b173626931543b5db2fa1c
Sabelodone <Sabelozondo825@gmail.com>
2023-02-21 01:55:18 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int char[] = '_', 'p', 'u', 't', 'c', 'h', 'a', 'r';
+
+	putchar("%c\n", '_', 'p', 'u', 't', 'c');
+	putchar("%c\n", 'h', 'a' 'r');
+	return (0);
+}
