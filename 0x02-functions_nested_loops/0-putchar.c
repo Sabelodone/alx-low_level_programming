4268ca30610fdcca1d661a4817a90b757c9ed17b
Sabelodone <Sabelozondo825@gmail.com>
2023-02-21 08:59:43 -0800

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,15 @@
  */
 int main(void)
 {
-	int char[] = '_', 'p', 'u', 't', 'c', 'h', 'a', 'r';
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	putchar("%c\n", '_', 'p', 'u', 't', 'c');
-	putchar("%c\n", 'h', 'a' 'r');
 	return (0);
 }
