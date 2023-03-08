c6e4c1f48caa3492606e3c5f161284efdb5f1865
Sabelodone <Sabelozondo825@gmail.com>
2023-03-07 23:54:31 -0800

    inception

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,49 +1,33 @@
 #include "main.h"
 
-/**
- * sqrt_helper - finds the square root of a number using binary search
- * @n: the number to find the square root of
- * @min: the lower bound of the binary search
- * @max: the upper bound of the binary search
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
- */
-int sqrt_helper(int n, int min, int max)
-{
-	int mid;
-
-	if (max < min)
-	{
-		return (-1);
-	}
-
-	mid = (min + max) / 2;
-
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		return (sqrt_helper(n, mid + 1, max));
-	}
-	else
-	{
-		return (sqrt_helper(n, min, mid - 1));
-	}
-}
+int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
+ * @n: number to calculate the square root of
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	return (sqrt_helper(n, 0, n));
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
