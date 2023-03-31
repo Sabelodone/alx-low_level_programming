5393e2007ed8ce0c1441c0c04cfe4f0a6776041b
Sabelodone <Sabelozondo825@gmail.com>
2023-03-31 01:55:26 -0700

    64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+section .data
+    msg db 'Hello, Holberton', 10, 0
+    fmt db '%s', 0
+
+section .text
+    global main
+    extern printf
+
+    main:
+        push rbp
+        mov rbp, rsp
+
+        mov rdi, fmt
+        mov rsi, msg
+        xor eax, eax
+        call printf
+
+        mov rsp, rbp
+        pop rbp
+        xor eax, eax
+        ret
+
