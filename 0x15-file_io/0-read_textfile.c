a08679a42c1c5ea8f136841eeaaa54c9be0345af
Sabelodone <Sabelozondo825@gmail.com>
2023-04-11 06:40:34 -0700

    a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,43 +4,45 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
-*read_textfile - Reads a file and prints its contents to the POSIX stdout.
-*@filename: the name of the file to read.
-*@letters: the maximum number of bytes to read and print.
+*read_textfile - Reads a text file and prints it to POSIX stdout.
+*@filename: A pointer to the name of the file.
+*@letters: the number of letters the function should read  and print.
 *
-*Return: the maximum number of bytesprinted, or 0 on failure.
+*Return: if the function fails or filename is NULL -o.
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
+int fd;
+ssize_t bytes_read, bytes_written;
+char *buffer;
 
 if (filename == NULL)
-return (-1);
+return (0);
 
-int fd = open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
-return (-1);
+return (0);
 
-char *buffer = malloc((letters + 1) * sizeof(char));
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
-
 {
 close(fd);
 return (0);
+
 }
 
-ssize_t bytes_read = read(fd, buffer, letters);
+bytes_read = read(fd, buffer, letters);
 if (bytes_read == -1)
 {
 
 free(buffer);
 close(fd);
 return (0);
-
 }
 
-ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
 
