cd837140196e9eb52335b0839224041a42a02db0
Sabelodone <Sabelozondo825@gmail.com>
2023-04-26 08:54:38 -0700

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -70,7 +70,42 @@ if (argc != 3)
 {
 
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
 }
 
-return (0);
+buffer = create_buffer(argv[2]);
+
+from = open(argv[1], O_RDONLY);
+if (from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
+to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (to == -1)
+{
+
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+do {
+r = read(from, buffer, 1024);
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+w = write(to, buffer, r);
+if (w == -1)
+{
+
+dprint(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+} while (r == 1024);
+
+close_file(from);
+close_file(to);
+
+free(buffer);
+return (0);
