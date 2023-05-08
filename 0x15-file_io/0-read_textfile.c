a528698d17a04e6c3b4de986253c0eff6eebe18e
Sabelodone <sabelozondo825@gmail.com>
2023-05-08 08:00:52 -0700

    second commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * read_textfile - reads a text file and writes its content to stdout.
@@ -9,7 +12,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nred, nwritten;
+	ssize_t nread, nwritten = 0;
 	int fd;
 	char *buf;
 
@@ -20,17 +23,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	bf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
 
 	close(fd);
-	retrn(0);
+	return (0);
 	}
 
 	nread = read(fd, buf, letters);
 	if (nread == -1)
-	}
+	{
 
 	free(buf);
 	close(fd);
@@ -38,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	nwritten = write(STDOUT_FILENO, buf, nread);
-	if (NWITTEN == -1 | NWRITTEN != nread)
+	if (nwritten == -1 | nwritten != nread)
 	{
 
 	free(buf);
