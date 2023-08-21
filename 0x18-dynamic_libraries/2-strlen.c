990bda6e97ea1bff66d9e67bccd9b6594eef26c3
Sabelodone <sabelozondo825gmail.com>
2023-08-21 08:24:31 -0700

    commit

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,15 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - function declaration
- * Description: find length of a string
- * @s: the string ptr
- * Return: index
- */
-
-int _strlen(char *s);
-
-/**
  * _strlen - function definition
  * Description: checks string length
  * @s: the string pointer
